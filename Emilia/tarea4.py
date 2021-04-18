c91f6768b4052a51ca439d917f8ec46a5242153f
Emiliarwr <77522030+Emiliarwr@users.noreply.github.com>
2021-04-17 23:10:23 -0400

    Tarea del orto 4
    banco de la nasa

--- Emilia/tarea4.py
+++ Emilia/tarea4.py
@@ -13,31 +13,69 @@ class Cajero:
         self.dinero = dinero
         self.banco = banco
 
-        def retirarDinero(self, dinero):
+    def retirarDinero(self, dinero):
+        if(dinero > self.dinero):
+            error()
+            self.accionCajero(2)
+        else:
             self.dinero -= dinero
+            self.succesfull()
+            self.volverAOperar()
 
-        def depositarDinero(self, dinero):
-            self.dinero += dinero
+    def succesfull(self):
+        return print(f"Transaccion exitosa, nuevo saldo disponible: {self.dinero}")
 
-        def cambiarCiudad(self, ciudad):
-            self.ciudad = ciudad
+    def depositarDinero(self, dinero):
+        self.dinero += dinero
+        self.succesfull()
+        self.volverAOperar()
 
-        def accionCajero(self, accion):
-            if(accion == 1):
-                print("¿Cuanto quieres depositar?")              
-            elif(accion == 2):
-                print("¿Cuanto quieres retirar?")
-            else:
-                print("Numero incorrecto, ingrese nuevamente.")
+    def cambiarCiudad(self, ciudad):
+        print(f"Cambiaste la ciudad de {self.ciudad} a {ciudad}")
+        self.ciudad = ciudad
+        self.volverAOperar()
 
+    def solicitarAccion(self):
+        accion = solicitarNumero()
+        self.accionCajero(accion)
+
+    def accionCajero(self, accion):
+        if(accion == 1):
+            print("¿Cuanto quieres depositar?")    
+            trans = solicitarNumero()
+            self.depositarDinero(trans)         
+        elif(accion == 2):
+            print("¿Cuanto quieres retirar?")
+            trans = solicitarNumero()
+            self.retirarDinero(trans)
+        elif(accion == 3):
+            print("¿A que ciudad quieres cambiar?")
+            ciudad = input()
+            self.cambiarCiudad(ciudad)
+        else:
+            error()
+            accion = solicitarNumero()
+            self.accionCajero(accion)
+            
+    def realizarOperacion(self):
+        return (f"¿Que transaccion quieres hacer? Saldo disponible: {self.dinero}\n1- Depositar\n2- Retirar\n3- Cambiar ciudad")
+
+    def volverAOperar(self):
+        print("¿Deseas realizar otra operacion? si/no")
+        respuesta = input()
+        if(respuesta == "si"):
+            print(self.realizarOperacion())
+            self.solicitarAccion()
+
+def error():
+    return print("Valor incorrecto, ingrese nuevamente.")
 
 def solicitarNumero():
     return int(input())
 
 def main():
     cajeroplaza = Cajero("Valparaiso", 150000, "Santander")
-    print(f"Bienvenido a Banco {cajeroplaza.banco} ubicado en {cajeroplaza.ciudad}\n¿Que transaccion quieres hacer? Saldo disponible: {cajeroplaza.dinero}\n1- Depositar\n2- Retirar")
-    accion = solicitarNumero()
-    cajeroplaza.accionCajero(accion)
+    print(f"Bienvenido a Banco {cajeroplaza.banco} ubicado en {cajeroplaza.ciudad}\n"+cajeroplaza.realizarOperacion())
+    cajeroplaza.solicitarAccion()
 
 main()
